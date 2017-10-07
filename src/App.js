020bccb90265dd9928ef33adb524073b0fdf2559
fragilehan <1634027084@qq.com>
2017-10-07 19:01:26 +0800

    test commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import KanbanBoardContainer from './KanbanBoardContainer';
+import './App.css';
+
+let cardsList = [
+{
+  id: 1,
+  title: "Read the Book",
+  description: "I should read the whole book",
+  color: '#bd8d31',
+  status: "in-progress",
+  tasks: []
+},
+{
+  id: 2,
+  title: "Write some code",
+  description: "Code along with the samples in the book",
+  status: "todo",
+  color: '#3a7e28',
+  tasks: [
+    {
+      id: 1,
+      name: "ContactList Example",
+      done: true
+    },
+     {
+      id: 2,
+      name: "kanban Example",
+      done: false
+    },
+     {
+      id: 3,
+      name: "My own experiments",
+      done: false
+    }
+
+  ]
+},
+
+]
+
+class App extends Component {
+  render() {
+    return (
+      <KanbanBoardContainer />
+    );
+  }
+}
+
+export default App;
